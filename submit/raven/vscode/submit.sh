cdde86787075e371445a7b4cd68744d926c48287
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-09-16 00:42:56 +0200

    Trying something out

---
+++ submit/raven/vscode/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/avion/%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/avion/%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name debug
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --constraint="gpu"
+
+#SBATCH --gres=gpu:2
+#SBATCH --mem=240000
+
+#SBATCH --time=23:59:59
+
+module purge
+module load avion
+
+sleep infinity
