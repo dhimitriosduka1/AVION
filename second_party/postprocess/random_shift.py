25f0de1c95caf487fe60edffa8df229fd1a8ab8d
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-10-18 17:00:28 +0200

    Clip segment if it exceeds original video length

--- second_party/postprocess/random_shift.py
+++ second_party/postprocess/random_shift.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import random
 import argparse
 import numpy as np
+import decord
+import wandb
 
 from tqdm import tqdm
 
@@ -18,6 +20,8 @@ def jitter_scale_window(
     scale_max: float = 1.6,
     min_duration: float = 1.0,
     max_duration: float = 5.0,
+    min_start: float = 0.0,
+    video_duration: float = 1.0,
 ):
     """
     Keep center fixed. Grow/shrink window by a random scale s ~ U[scale_min, scale_max].
@@ -32,13 +36,27 @@ def jitter_scale_window(
     s = random.uniform(scale_min, scale_max)
     new_d = max(min(d * s, max_duration), min_duration)
 
-    new_start = c - new_d / 2.0
+    new_start = max(c - new_d / 2.0, min_start)
     new_end = c + new_d / 2.0
+
+    if new_end > video_duration:
+        print(f"New end is greater than video duration: {new_end} > {video_duration}")
+
+        new_start += new_end - video_duration
+        new_end = video_duration
+
     return new_start, new_end
 
 
 def main(args):
     assert args.dataset.endswith(".pkl"), "Dataset must be a pickle file"
+
+    wandb.init(
+        project="Thesis",
+        name=f"Random Shift - {args.scale_min} - {args.scale_max} - {args.min_duration} - {args.max_duration}",
+        config={**args.__dict__},
+        group=f"Random Timestamp Shift",
+    )
 
     print(f"Opening {args.dataset} dataset")
     with open(args.dataset, "rb") as f:
@@ -49,37 +67,67 @@ def main(args):
     new_timestamps_duration = []
 
     new_data = []
-    for sample in tqdm(data, desc="Jittering timestamps"):
+    prev_video_id = None
+    for i, sample in tqdm(enumerate(data), desc="Jittering timestamps"):
+        if prev_video_id != sample[0]:
+            vr = decord.VideoReader(os.path.join(args.video_root, f"{sample[0]}.mp4"))
+            video_duration = len(vr) / vr.get_avg_fps()
+            prev_video_id = sample[0]
+
         new_start, new_end = jitter_scale_window(
             sample[1],
             sample[2],
-            scale_min=0.1,
-            scale_max=20.0,
-            min_duration=1.0,
-            max_duration=5.0,
+            scale_min=args.scale_min,
+            scale_max=args.scale_max,
+            min_duration=args.min_duration,
+            max_duration=args.max_duration,
+            video_duration=video_duration,
         )
         old_timestamps_duration.append(sample[2] - sample[1])
         new_timestamps_duration.append(new_end - new_start)
         new_data.append((sample[0], new_start, new_end, sample[3]))
 
-    print(f"Old timestamps duration: {np.mean(old_timestamps_duration)}")
-    print(f"Old timestamps duration: {np.std(old_timestamps_duration)}")
-
-    print(f"New timestamps duration: {np.mean(new_timestamps_duration)}")
-    print(f"New timestamps duration: {np.std(new_timestamps_duration)}")
-
+        wandb.log(
+            {
+                "progress": i / len(data),
+            }
+        )
     with open(
         os.path.join(
-            Path(args.output_path) / "random_shift_timestamps", "ego4d_train_random_shift.pkl"
+            Path(args.output_path) / "random_shift_timestamps",
+            "ego4d_train_random_shift.pkl",
         ),
         "wb",
     ) as f:
         pickle.dump(new_data, f)
+
+    table = wandb.Table(columns=["Metric", "Mean", "Std"])
+
+    table.add_data(
+        "Timestamp Duration (Original)",
+        np.mean(old_timestamps_duration),
+        np.std(old_timestamps_duration),
+    )
+
+    table.add_data(
+        "Timestamp Duration (Jittered)",
+        np.mean(new_timestamps_duration),
+        np.std(new_timestamps_duration),
+    )
+
+    wandb.log({"table": table})
+    wandb.finish()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True)
     parser.add_argument("--output-path", type=str, required=True)
+    parser.add_argument("--video-root", type=str, required=True)
+    parser.add_argument("--scale-min", type=float, default=0.1)
+    parser.add_argument("--scale-max", type=float, default=20.0)
+    parser.add_argument("--min-duration", type=float, default=1.0)
+    parser.add_argument("--max-duration", type=float, default=5.0)
     args = parser.parse_args()
+
     main(args)
