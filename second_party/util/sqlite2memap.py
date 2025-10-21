294814caebd1853443ddf1aea17f765701bd70d7
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-10-21 08:56:07 +0200

    WIP

--- second_party/util/sqlite2memap.py
+++ second_party/util/sqlite2memap.py
@@ -11,34 +11,40 @@ from second_party.storage.memmap import write_memmap, read_memmap
 def main(args):
     assert args.sqlite_path.endswith(".sqlite"), "SQLite file must end with .sqlite"
 
-    print(f"Loading embeddings from {args.sqlite_path}")
-    embeddings_matrix = []
     captions = {}
 
     with SQLiteClient(args.sqlite_path) as client:
+        total_embeddings = client.count_embeddings()
+        shape = (total_embeddings, 1280)
+        print(f"Shape: {shape}")
+        memmap = np.memmap(
+            Path(args.memap_output_path) / "embeddings.memmap", shape=shape, mode="w+"
+        )
+
         batch_idx = 0
         batch_size = 10_000
-        print(f"Total number of embeddings: {client.count_embeddings()}")
-        total_batches = client.count_embeddings() // batch_size
+
+        print(f"Total number of embeddings: {total_embeddings}")
+        total_batches = total_embeddings // batch_size
+
         for batch in client.iter_embeddings(batch_size=batch_size):
-            print(f"Processing batch {batch_idx} of {total_batches}")
+            print(f"Processing batch {batch_idx + 1} of {total_batches}")
             for i, (caption, embedding) in enumerate(batch):
                 captions[caption] = batch_idx * batch_size + i
-                embeddings_matrix.append(embedding)
+                memmap[batch_idx * batch_size + i] = embedding
 
-            batch_idx += 1 
+            batch_idx += 1
 
-    print(f"Loaded {len(embeddings_matrix)} embeddings")
-    embeddings_matrix = np.array(embeddings_matrix)
+            if batch_idx % 100 == 0:
+                memmap.flush()
 
-    print(f"Saving embeddings to {Path(args.memap_output_path) / 'embeddings.memmap'}")
-    write_memmap(Path(args.memap_output_path) / "embeddings.memmap", embeddings_matrix)
+        memmap.flush()
 
     with open(Path(args.memap_output_path) / "captions.json", "w") as f:
         json.dump(captions, f)
 
     with open(Path(args.memap_output_path) / "shape.json", "w") as f:
-        json.dump({"shape": embeddings_matrix.shape}, f)
+        json.dump({"shape": shape}, f)
 
 
 if __name__ == "__main__":
@@ -47,4 +53,5 @@ if __name__ == "__main__":
     parser.add_argument("--memap-output-path", type=str, required=True)
     args = parser.parse_args()
 
-    # main(args)
+    print(args)
+    main(args)
