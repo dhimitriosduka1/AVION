056da74cb6d8259c7caee8da2540cff8d5984f34
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-10-23 16:46:45 +0200

    Add seed to filename

--- second_party/postprocess/random_shift.py
+++ second_party/postprocess/random_shift.py
@@ -211,7 +211,7 @@ def main(args):
     with open(
         os.path.join(
             Path(args.output_path) / "random_shift_timestamps",
-            f"ego4d_train_random_shift_{args.scale_min}_{args.scale_max}_{args.min_duration}_{args.max_duration}.pkl",
+            f"ego4d_train_random_shift_{args.scale_min}_{args.scale_max}_{args.min_duration}_{args.max_duration}_{args.seed}.pkl",
         ),
         "wb",
     ) as f:
