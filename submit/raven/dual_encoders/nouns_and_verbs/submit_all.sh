48b807f86cf917eca3b7122b0bbe8efe714a5911
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-11-10 13:09:40 +0100

    Script for noun and verbs

---
+++ submit/raven/dual_encoders/nouns_and_verbs/submit_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+gaps=(1.1 1.4 1.7 2.0 2.3 2.6 2.9 3.0 4.0)
+
+for gap in "${gaps[@]}"; do 
+  gap_nodot="${gap/./}"
+  sbatch \
+    --job-name "nouns_and_verbs_gap_${gap_nodot}" \
+    -o "nouns_and_verbs_gap_${gap_nodot}_%A_%a_%x_%j_%N.out" \
+    -e "nouns_and_verbs_gap_${gap_nodot}_%A_%a_%x_%j_%N.err" \
+    --export=ALL,GAP="${gap}" \
+    /u/dduka/work/projects/Thesis/AVION/submit/raven/dual_encoders/nouns_and_verbs/script.sbatch
+done
