f9c3a5bbab102c0ad6b119958744476e87909b02
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-11-11 01:12:41 +0100

    Scripts

---
+++ submit/raven/postprocess/symmetric_additive_offset/symmetric_additive_offset_3.5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+#SBATCH -o /ptmp/dduka/work/logs/avion/symmetric_additive_offset_%A_%a_%x_%j_%N.out
+#SBATCH -e /ptmp/dduka/work/logs/avion/symmetric_additive_offset_%A_%a_%x_%j_%N.err
+
+#SBATCH --job-name symmetric_additive_offset
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=72
+
+#SBATCH --time=01:59:59
+#SBATCH --partition=standard
+
+# Load required modules
+module purge
+module load anaconda/3/2021.11
+
+# Activate the conda environment
+conda activate avion
+
+cd /u/dduka/work/projects/Thesis/AVION
+
+export PYTHONPATH=.:third_party/decord/python/
+
+python3 -m second_party.postprocess.symmetric_additive_offset \
+    --dataset /ptmp/dduka/databases/ego4d/ego4d_train.pkl \
+    --output-path /ptmp/dduka/databases/ego4d/rewritten_timestamps/ \
+    --video-root /ptmp/dduka/databases/ego4d/ego4d/v2/full_scale/ \
+    --offset 3.5 \
+    --num-workers 70 \
