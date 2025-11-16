516f407f9889022f4e3fa42d8d3349a4aec9f1d3
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2025-11-16 13:45:40 +0100

    Scripts for preprocessing IoU

---
+++ submit/dais/postprocess/iou/submit.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+#SBATCH -o /dais/fs/scratch/dduka/logs/avion/iou_%A_%a_%x_%j_%N.out
+#SBATCH -e /dais/fs/scratch/dduka/logs/avion/iou_%A_%a_%x_%j_%N.err
+
+#SBATCH -J iou
+#SBATCH --time=00:59:59
+
+#SBATCH --nodes=1
+#SBATCH --partition="gpu1"
+#SBATCH --cpus-per-task=12
+#SBATCH --threads-per-core=1
+
+#SBATCH --gres=gpu:h200:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=250000
+
+#SBATCH --array=0-5
+
+set -euo pipefail
+
+module purge
+micromamba activate avion_fa2
+
+nvidia-smi
+
+IoUs=(0.005 0.01 0.015 0.02 0.025 0.03)
+IoU="${IoUs[$SLURM_ARRAY_TASK_ID]}"
+
+# Set up distributed training environment variables
+export MASTER_PORT=$((12000 + RANDOM % 20000))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+
+export CUDA_VISIBLE_DEVICES=0
+
+# Debug: Print GPU and node information
+echo "Job running on nodes: $SLURM_JOB_NODELIST"
+echo "Total nodes: ${SLURM_NNODES:-1}"
+echo "GPUs per node: ${SLURM_GPUS_ON_NODE:-unknown}"
+echo "Array task ID: $SLURM_ARRAY_TASK_ID  -> OFFSET: $IoU"
+
+cd /u/dduka/project/AVION
+export PYTHONPATH=.:third_party/decord/python/
+
+python3 /u/dduka/project/AVION/second_party/iou/main.py --min-iou ${IoU}
---
+++ submit/dais/postprocess/iou_from_scaled/submit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -l
+#SBATCH -o /dais/fs/scratch/dduka/logs/avion/iou_s_%A_%a_%x_%j_%N.out
+#SBATCH -e /dais/fs/scratch/dduka/logs/avion/iou_s_%A_%a_%x_%j_%N.err
+
+#SBATCH -J iou_s
+#SBATCH --time=00:59:59
+
+#SBATCH --nodes=1
+#SBATCH --partition="gpu1"
+#SBATCH --cpus-per-task=12
+#SBATCH --threads-per-core=1
+
+#SBATCH --gres=gpu:h200:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=250000
+
+#SBATCH --array=0-5
+
+set -euo pipefail
+
+module purge
+micromamba activate avion_fa2
+
+nvidia-smi
+
+IoUs=(0.005 0.01 0.015 0.02 0.025 0.03)
+IoU="${IoUs[$SLURM_ARRAY_TASK_ID]}"
+
+# Set up distributed training environment variables
+export MASTER_PORT=$((12000 + RANDOM % 20000))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+
+export CUDA_VISIBLE_DEVICES=0
+
+# Debug: Print GPU and node information
+echo "Job running on nodes: $SLURM_JOB_NODELIST"
+echo "Total nodes: ${SLURM_NNODES:-1}"
+echo "GPUs per node: ${SLURM_GPUS_ON_NODE:-unknown}"
+echo "Array task ID: $SLURM_ARRAY_TASK_ID  -> OFFSET: $IoU"
+
+cd /u/dduka/project/AVION
+export PYTHONPATH=.:third_party/decord/python/
+
+python3 /u/dduka/project/AVION/second_party/iou/main.py \
+    --dataset /dais/fs/scratch/dduka/databases/ego4d/random_shift/ego4d_train_random_shift_1.4_1.4_1.0_42_enriched.pkl \
+    --min-iou ${IoU} \
+    --postfix "From Scaled 1.4" \
+    --out-path /dais/fs/scratch/dduka/databases/ego4d/iou_from_scaled/
