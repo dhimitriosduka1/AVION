3eb74f9a00c9160913bc72d960472e3c40c0320d
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2026-01-28 18:20:00 +0100

    Scripts for lavila

--- second_party/utils/scripts/generate_lavila_2.1_scaled.sh
+++ second_party/utils/scripts/generate_lavila_2.1_scaled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-python3 /u/dduka/project/AVION/second_party/utils/create_lavila_rephrased_dataset_with_uuid.py \
+python3 /u/dduka/project/AVION/second_party/utils/copy_timestamps_to_lavila_dataset.py \
     --source_timestamps "/dais/fs/scratch/dduka/databases/ego4d/random_shift/ego4d_train_random_shift_2.1_2.1_1.0_with_uuid.pkl" \
     --lavila_rephrased "/dais/fs/scratch/dduka/databases/ego4d/ego4d_train.rephraser.no_punkt_top3_with_uuid.pkl" \
     --out_with_uuid "/dais/fs/scratch/dduka/databases/ego4d/ego4d_train.rephraser.no_punkt_top3_refined_2.1_scaled_with_uuid.pkl" \
---
+++ second_party/utils/scripts/generate_vllm_original_ego4d_1_caption.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+python3 /u/dduka/project/AVION/second_party/utils/copy_timestamps_to_lavila_dataset.py \
+    --source_timestamps "/dais/fs/scratch/dduka/databases/ego4d/qwen_refinement/standard/pickle/ego4d_train_standard_1_caption_vllm_with_uuid.pkl" \
+    --lavila_rephrased "/dais/fs/scratch/dduka/databases/ego4d/ego4d_train.rephraser.no_punkt_top3_with_uuid.pkl" \
+    --out_with_uuid "/dais/fs/scratch/dduka/databases/ego4d/qwen_refinement/standard/pickle/ego4d_train.rephraser.no_punkt_top3_refined_qwen_1_caption_vllm_with_uuid.pkl" \
+    --out_refined "/dais/fs/scratch/dduka/databases/ego4d/qwen_refinement/standard/pickle/ego4d_train.rephraser.no_punkt_top3_refined_qwen_1_caption_vllm.pkl"
