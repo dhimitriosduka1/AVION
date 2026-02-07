2aefc393cd1252e6c143404bbf4f68902b358bbb
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2026-02-07 10:22:37 +0100

    WIP

--- submit/dais/qwen3vl/standard_ttrv_scaled_400th_step.sh
+++ submit/dais/qwen3vl/standard_ttrv_scaled_400th_step.sh
@@ -5,7 +5,7 @@
 
 #SBATCH -J ttrv_scaled_standard
 
-#SBATCH --time=15:59:59
+#SBATCH --time=13:59:59
 #SBATCH --nodes=1
 #SBATCH --partition="gpu1"
 #SBATCH --gres=gpu:h200:4
@@ -13,7 +13,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=1000000
 
-#SBATCH --array=0-40%8
+#SBATCH --array=0-20%10
 
 cd /u/dduka/project/AVION/
 
@@ -41,8 +41,8 @@ module load cuda/12.8
 uv run ./second_party/qwen3vl/vllm_refine.py \
     --start_idx $START_IDX \
     --end_idx $END_IDX \
-    --batch_size 512 \
+    --batch_size 2048 \
     --tensor_parallel_size 4 \
-    --output_file /dais/fs/scratch/dduka/databases/ego4d/qwen_refinement/standard/output_1_caption_ttrv_form_standard_checkpoint_200.jsonl \
-    --pkl_path /dais/fs/scratch/dduka/databases/ego4d/ego4d_train_with_uuid.pkl \
-    --model_path /dais/fs/scratch/dduka/training_metadata/ttrv/checkpoints/TTRL-verl/tag-Qwen/Qwen3-VL-8B-Instruct/TTRL-EGO4D-TAR-STANDARD-SEGMENTS-grpo/global_step_200/actor_merged/
+    --output_file /dais/fs/scratch/dduka/databases/ego4d/qwen_refinement/scaled/output_1_caption_ttrv_form_standard_checkpoint_400.jsonl \
+    --pkl_path /dais/fs/scratch/dduka/databases/ego4d/random_shift/ego4d_train_random_shift_2.1_2.1_1.0_with_uuid.pkl \
+    --model_path /dais/fs/scratch/dduka/training_metadata/ttrv/checkpoints/TTRL-verl/tag-Qwen/Qwen3-VL-8B-Instruct/TTRL-EGO4D-TAR-STANDARD-SEGMENTS-grpo/global_step_400/actor_merged/
